7c346bbfc24f9609deed20aaa4c13336938893d2
Alberto Bordonado <alberto.bordonado@gmail.com>
2021-12-08 22:21:15 +0100

    Initial commit

---
+++ src/main/java/com/abordonado/ABetting/ABettingApplication.java
@@ -0,0 +1,13 @@
+package com.abordonado.ABetting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ABettingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ABettingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abordonado/ABetting/ABettingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abordonado.ABetting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ABettingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
